e689bc94ae96f8eb81a9a50cd41b53dae3023c89
Harsh Vardhan Singh <harsh00var@gmail.com>
2024-06-28 20:56:42 +0530

    fix 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Banner from "./components/Banner";
 import ImageSlider from "./components/ImageSlider";
-import ServiceCards from "./components/ServiceCard";
-
 export default function App() {
   return (
     <>
