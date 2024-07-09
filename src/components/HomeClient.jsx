4ef44346c56747e828b208c1dbd404dea79c428d
Harsh Vardhan Singh <harsh00var@gmail.com>
2024-07-09 19:33:54 +0530

    fix

--- src/components/HomeClient.jsx
+++ src/components/HomeClient.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, useMediaQuery } from '@mui/material';
 import { keyframes } from '@emotion/react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline'
 import DividerImage from '../assets/img/client_divider.png';
 
 import Client1 from '../assets/img/client1.png';
-// import Client2 from '../assets/img/client2.png';
-// import Client3 from '../assets/img/client3.png';
 import Client4 from '../assets/img/client4.png';
-// import Client5 from '../assets/img/client5.png';
 import Client6 from '../assets/img/client6.png';
-// import Client7 from '../assets/img/client7.png';
 import Client8 from '../assets/img/client8.png';
-// import Client9 from '../assets/img/client9.png';
-// import Client10 from '../assets/img/client10.png';
-// import Client11 from '../assets/img/client11.png';
-// import Client12 from '../assets/img/client12.png';
 import Client13 from '../assets/img/client13.png';
 import Client14 from '../assets/img/client14.png';
 import Client15 from '../assets/img/client15.png';
 import Client16 from '../assets/img/client16.png';
-// import Client17 from '../assets/img/client17.png';
-// import Client18 from '../assets/img/client18.png';
-// import Client19 from '../assets/img/client19.png';
-
 
 const theme = createTheme({
     typography: {
@@ -43,6 +31,8 @@ const clients = [
   `;
   
   const ClientsCarousel = () => {
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+    
     return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
