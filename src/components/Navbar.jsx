8770cc6dc2127419ddb4b97ac7225724faf0c206
Harsh Vardhan Singh <harsh00var@gmail.com>
2024-07-10 13:34:56 +0530

    navbar responsive fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,55 +13,59 @@ function Navbar() {
       <div className="sticky-navbar">
         <div className="navbar bg-base-100 mt-2">
           {isMobile ? (
-            <div className="flex justify-between items-center w-full px-4">
-              <a href="/" className="ml-4">
-                <img src={logo} alt="logo" />
-              </a>
-              <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h8m-8 6h16"
-                    />
-                  </svg>
-                </div>
-                <ul
-                  tabIndex={0}
-                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-                >
-                  <li>
-                    <a href="/aboutus" className="montserrat-medium">About</a>
-                  </li>
-                  <li>
-                    <a className="montserrat-medium">Services</a>
-                  </li>
-                  <li>
-                    <Link to='/projectsectorial' className="montserrat-medium">Portfolio Projects</Link>
-                  </li>
-                  <li>
-                    <a href="/allprojects" className="montserrat-medium">All Project</a>
-                  </li>
-                  <li>
-                    <a href="/contact" className="montserrat-medium">Contact</a>
-                  </li>
-                  <li>
-                    <a href="/enquire" className="montserrat-medium">Enquire</a>
-                  </li>
-                </ul>
+            <>
+              <div className="navbar-start">
+                <a href="/" className="ml-4">
+                  <img src={logo} alt="logo" className="h-8 w-auto" /> {/* Adjusted logo size */}
+                </a>
               </div>
-            </div>
+              <div className="navbar-end mr-4">
+                <div className="dropdown">
+                  <div tabIndex={0} role="button" className="btn btn-ghost">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h8m-8 6h16"
+                      />
+                    </svg>
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow right-0" // Adjusted position
+                  >
+                    <li>
+                      <a href="/aboutus" className="montserrat-medium">About</a>
+                    </li>
+                    <li>
+                      <a className="montserrat-medium">Services</a>
+                    </li>
+                    <li>
+                      <Link to='/projectsectorial' className="montserrat-medium">Portfolio Projects</Link>
+                    </li>
+                    <li>
+                      <a href="/allprojects" className="montserrat-medium">All Project</a>
+                    </li>
+                    <li>
+                      <a href="/contact" className="montserrat-medium">Contact</a>
+                    </li>
+                    <li>
+                      <a href="/enquire" className="montserrat-medium">Enquire</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </>
           ) : (
             <>
-              <div className="navbar-start flex items-center">
+              <div className="navbar-start">
                 <div className="dropdown">
                   <div
                     tabIndex={0}
@@ -113,7 +117,11 @@ function Navbar() {
                   </h3>
                 </div>
               </div>
+            </>
+          )}
 
+          {!isMobile && (
+            <>
               <div className="navbar-right hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                   <li>
