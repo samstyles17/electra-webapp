86ec5cc2e49acd541d9452aa877f687a721ef226
Harsh Vardhan Singh <harsh00var@gmail.com>
2024-07-14 22:59:13 +0530

    final fixes

--- src/components/PartnerSection.jsx
+++ src/components/PartnerSection.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles.css';
-import partnerImage from "../assets/img/partnerimg.png" // Adjust the path if necessary
+import partnerImage from "../assets/img/partnerimg.png"; // Adjust the path if necessary
+import partnerImagePortrait from "../assets/img/partner_section_potrait_img.png"; // Adjust the path if necessary
 
 const PartnerSection = () => {
     return (
         <div className="partner-section">
             <img src={partnerImage} alt="Partner" className="partner-image" />
+            <img src={partnerImagePortrait} alt="Partner Portrait" className="partner-image-portrait" />
         </div>
     );
 };
--- src/components/SectionComponent.jsx
+++ src/components/SectionComponent.jsx
@@ -1,68 +1,96 @@
 import React from 'react';
-import '../styles.css';
+import { Grid, Box, Typography, Divider, useMediaQuery, useTheme } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import '@fontsource/montserrat';
+
+const customTheme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat, sans-serif',
+  },
+});
 
 function SectionComponent() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <div className="section-component-container">
-      <div className="section-component-row">
-        <div className="section-component-text-section">
-          <h1 className="section-component-heading montserrat-medium">
-            Driving Change,
-          </h1>
-          <h1 className="section-component-sub-heading montserrat-medium">
-            Powering Possibilities
-          </h1>
-          <p className="section-component-paragraph montserrat-regular">
-            We are catalysts for change. By pushing the boundaries of what's possible
-            and harnessing the power of imagination, we not only drive change but
-            also empower endless possibilities for our clients and partners.
-          </p>
-        </div>
-      </div>
-      <div className="section-component-row">
-        <div className="section-component-stats-container">
-          <div className="section-component-stat-item">
-            <h2 className="section-component-stat-number text-orange">12+</h2>
-            <p className="section-component-stat-description">Years of Experience</p>
-          </div>
-          <div className="section-component-stat-divider"></div>
-          <div className="section-component-stat-item">
-            <h2 className="section-component-stat-number section-component-text-orange">300+</h2>
-            <p className="section-component-stat-description">Projects Delivered</p>
-          </div>
-          <div className="section-component-stat-divider"></div>
-          <div className="section-component-stat-item">
-            <h2 className="section-component-stat-number text-orange">250+</h2>
-            <p className="section-component-stat-description">Happy Clients</p>
-          </div>
-        </div>
-        <div className="section-component-vision-container">
-          <div className="section-component-vision-box">
-            <h2 className="section-component-vision-title montserrat-medium"><span className="section-component-dot"></span> OUR VISION</h2>
-            <p className="section-component-vision-paragraph montserrat-medium">
-              We envision leading the electrical industry with innovation and excellence.
-              Rooted in trust and transparency, we aim to deliver tailored solutions
-              that surpass expectations, empowering industries and communities with safe,
-              reliable, and sustainable electrical solutions.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="section-component-row">
-        <div className="section-component-mission-container">
-          <div className="section-component-mission-box">
-            <h2 className="section-component-mission-title montserrat-medium"><span className="section-component-dot section-component-blue-dot"></span> OUR MISSION</h2>
-            <ul className="section-component-mission-list montserrat-medium">
-              <li>Delivering exceptional high-tension electrical contracting services.</li>
-              <li>Ensuring safety and sustainability with clear communication in all projects.</li>
-              <li>Exceeding customer expectations through continuous improvement.</li>
-              <li>Powering progress and enhancing community life with reliable electrical solutions.</li>
-              <li>Pioneering cutting-edge technologies and methodologies.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', marginBottom: '100px', backgroundColor: '#fff', color: '#333' }}>
+        <Grid container spacing={isMobile ? 2 : 3} alignItems="flex-start">
+          <Grid item xs={12} md={6}>
+            <Box sx={{ marginRight: isMobile ? 0 : '40px', textAlign: 'left', marginBottom: '40px', marginLeft: isMobile ? 0 : '40px' }}>
+              <Typography variant="h3" sx={{ color: '#3a3a3a', transition: 'color 0.3s ease-in-out' }}>
+                Driving Change,
+              </Typography>
+              <Typography variant="h3" sx={{ color: '#f0f0f0', marginTop: '0px', marginBottom: '40px', transition: 'color 1s ease-in-out', '&:hover': { color: '#000' } }}>
+                Powering Possibilities
+              </Typography>
+              <Typography variant="body1" sx={{ fontSize: '1rem', lineHeight: 1.6 }}>
+                We are catalysts for change. By pushing the boundaries of what's possible and harnessing the power of imagination, we not only drive change but also empower endless possibilities for our clients and partners.
+              </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start', marginLeft: isMobile ? '-10px' : '20px', flexWrap: isMobile ? 'wrap' : 'nowrap' }}>
+              <Box sx={{ textAlign: 'left', margin: isMobile ? '0 5px 10px' : '0 25px', flex: isMobile ? '1 0 45%' : 'none', padding: isMobile ? '10px' : '0', backgroundColor: isMobile ? '#f5f5f5' : 'transparent', borderRadius: isMobile ? '10px' : '0' }}>
+                <Typography variant="h5" sx={{ color: '#007bff', margin: 0, fontWeight: 500 }}>
+                  12+
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: isMobile ? '12px' : '16px', margin: 0, color: '#333' }}>
+                  Years of Experience
+                </Typography>
+              </Box>
+              {!isMobile && <Divider orientation="vertical" flexItem sx={{ mx: 1 }} />}
+              <Box sx={{ textAlign: 'left', margin: isMobile ? '0 5px 10px' : '0 25px', flex: isMobile ? '1 0 45%' : 'none', padding: isMobile ? '10px' : '0', backgroundColor: isMobile ? '#f5f5f5' : 'transparent', borderRadius: isMobile ? '10px' : '0' }}>
+                <Typography variant="h5" sx={{ color: '#ff6600', margin: 0, fontWeight: 500 }}>
+                  300+
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: isMobile ? '12px' : '16px', margin: 0, color: '#333' }}>
+                  Projects Delivered
+                </Typography>
+              </Box>
+              {!isMobile && <Divider orientation="vertical" flexItem sx={{ mx: 1 }} />}
+              <Box sx={{ textAlign: 'left', margin: isMobile ? '0 5px 10px' : '0 25px', flex: isMobile ? '1 0 100%' : 'none', padding: isMobile ? '10px' : '0', backgroundColor: isMobile ? '#f5f5f5' : 'transparent', borderRadius: isMobile ? '10px' : '0' }}>
+                <Typography variant="h5" sx={{ color: '#007bff', margin: 0, fontWeight: 500 }}>
+                  250+
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: isMobile ? '12px' : '16px', margin: 0, color: '#333' }}>
+                  Happy Clients
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box sx={{ marginBottom: '50px', marginLeft: isMobile ? '0' : '60px', marginRight: isMobile ? '0' : '10px', maxWidth: '500px' }}>
+              <Box sx={{ backgroundColor: isMobile ? '#f5f5f5' : '#ffffff', padding: '30px', borderRadius: '15px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)', width: '100%', textAlign: 'left' }}>
+                <Typography variant="h6" sx={{ color: '#ff6600', marginBottom: '10px', marginTop: '10px' }}>
+                  <Box component="span" sx={{ backgroundColor: '#ff5733', width: '10px', height: '10px', borderRadius: '15px', display: 'inline-block', marginRight: '10px' }} />
+                  OUR VISION
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: '0.9rem', lineHeight: 2.5, color: '#3a3a3a' }}>
+                  We envision leading the electrical industry with innovation and excellence. Rooted in trust and transparency, we aim to deliver tailored solutions that surpass expectations, empowering industries and communities with safe, reliable, and sustainable electrical solutions.
+                </Typography>
+              </Box>
+            </Box>
+            <Box sx={{ marginLeft: isMobile ? '0' : '60px', marginTop: '10px', marginRight: isMobile ? '0' : '10px', maxWidth: '500px' }}>
+              <Box sx={{ backgroundColor: isMobile ? '#f5f5f5' : '#ffffff', padding: '30px', borderRadius: '15px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)', width: '100%', textAlign: 'left' }}>
+                <Typography variant="h6" sx={{ color: '#007bff', marginBottom: '10px', marginTop: '10px' }}>
+                  <Box component="span" sx={{ backgroundColor: '#007bff', width: '10px', height: '10px', borderRadius: '15px', display: 'inline-block', marginRight: '10px' }} />
+                  OUR MISSION
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: '0.9rem', lineHeight: 2.5, color: '#3a3a3a' }}>
+                  <ul>
+                    <li>Delivering exceptional high-tension electrical contracting services.</li>
+                    <li>Ensuring safety and sustainability with clear communication in all projects.</li>
+                    <li>Exceeding customer expectations through continuous improvement.</li>
+                    <li>Powering progress and enhancing community life with reliable electrical solutions.</li>
+                    <li>Pioneering cutting-edge technologies and methodologies.</li>
+                  </ul>
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
   );
 }
 
