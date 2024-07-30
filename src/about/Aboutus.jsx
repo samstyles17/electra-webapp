3b6db50be7b4e3dfa0e6b61f514e35498b5ce688
kushalsourav <kushalsourav2@gmail.com>
2024-07-30 19:06:46 +0530

    fix: about

--- src/about/Aboutus.jsx
+++ src/about/Aboutus.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import AboutSec1 from "../components/AboutSec1";
 import Navbar from "../components/Navbar";
 import Vision from "../components/Vision";
@@ -11,20 +11,42 @@ import PartnerSection from "../components/PartnerSection";
 import Footer from "../components/Footer";
 import SectionComponent from "../components/SectionComponent";
 
-
 function Aboutus() {
+  const rowRefs = useRef([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.1 });
+
+    rowRefs.current.forEach(row => {
+      if (row) observer.observe(row);
+    });
+
+    return () => {
+      rowRefs.current.forEach(row => {
+        if (row) observer.unobserve(row);
+      });
+    };
+  }, []);
+
   return (
     <>
-        <Navbar />
-        <div className="aboutus">
-        <AboutSec1 />
-        <SectionComponent/>
-        <WhyChoose />
-        <OurTeam />
-        <OurPeople/>
-        <IntegrityCarousel/>
-        <PartnerSection/>
-        <Footer/>
+      <Navbar />
+      <div className="aboutus">
+        <div ref={el => rowRefs.current[0] = el}><AboutSec1 /></div>
+        <div ref={el => rowRefs.current[1] = el}><SectionComponent/></div>
+        <div ref={el => rowRefs.current[2] = el}><WhyChoose /></div>
+        <div ref={el => rowRefs.current[3] = el}><OurTeam /></div>
+        <div ref={el => rowRefs.current[4] = el}><OurPeople/></div>
+        <div ref={el => rowRefs.current[5] = el}><IntegrityCarousel/></div>
+        <div ref={el => rowRefs.current[6] = el}><PartnerSection/></div>
+        <div ref={el => rowRefs.current[7] = el}><Footer /></div>
       </div>
     </>
   );
