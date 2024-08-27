3c8fdc977d6db235a94c82b5622b6c506b62603b
Harsh Vardhan Singh <harsh00var@gmail.com>
2024-08-27 11:20:46 +0530

    updated partner image in about us page - desktop and mobile version

--- src/components/PartnerSection.jsx
+++ src/components/PartnerSection.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+// import React from 'react';
 import '../styles.css';
-import partnerImage from "../assets/img/partnerimg.png"; // Adjust the path if necessary
-import partnerImagePortrait from "../assets/img/partner_section_potrait_img.png"; // Adjust the path if necessary
+import partnerImage from "../assets/img/patnerimgnew.png"; // Adjust the path if necessary
+import partnerImagePortrait from "../assets/img/partneraboutpotrait.png"; // Adjust the path if necessary
 
 const PartnerSection = () => {
     return (
